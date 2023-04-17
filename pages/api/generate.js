0c4a631dce7d46fb7594309e7edb1c0660d2f7d3
Stephenson30 <96519899+Stephenson30@users.noreply.github.com>
2023-04-17 03:54:52 -0700

    vpn

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -7,12 +7,10 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix = 
-`you are pathologist, who have a good skills of interpreting 
-stained body tissues and cells. Interpret the result of the 
-stained tissues below that have been examined under a light microscope
-.Base on their interaction with the special stain stating the special 
-stain used, abnormalities if found, the type of  tissues or cells present,
- and diseases the abnormalities might caused.`;
+`you are pathologist, who have a good skills of interpreting stained body tissues and cells.
+Interpret the result of the stained tissues below that have been examined under a light microscope.
+Base on their interaction with the special stain stating the special stain used, abnormalities if found, the type of  tissues or cells present,
+and diseases the abnormalities might caused. if no question was provided , request for questions.`;
 
 const generateAction = async (req, res) => {
   // Run first prompt
@@ -22,7 +20,7 @@ const generateAction = async (req, res) => {
     model: 'text-davinci-003',
     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
     temperature: 0.1,
-    max_tokens: 1050,
+    max_tokens: 1056,
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
